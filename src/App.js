3859b9f1f92afd2d5e3d013f81990f376d60fb1d
L3x <hleon0507@gmail.com>
2022-10-14 00:05:45 -0500

    ajustando columnas y filas para la card

--- src/App.js
+++ src/App.js
@@ -20,13 +20,43 @@ function App() {
       title:'Para ti',
       image:'https://m.media-amazon.com/images/I/71tQckGUpFL._AC_SL1500_.jpg',
     }
-
-
   ]
+
+
+function Card (){
+  return(
+
+  <div className='card'>
+      <div className='card-body'>
+        <div className='col'>
+          <div className='row-6'>
+            <img  src='https://w0.peakpx.com/wallpaper/312/783/HD-wallpaper-person-holding-black-umbrella.jpg' alt=''/>,
+            <button className='card__btn'>SHOP</button>
+          </div>
+          
+          <div className='row-6'>
+            <img src='https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61PyxsMIxlL._AC_SL1280_.jpg' alt=''/>,
+            <h2 className='card-title'>Pufi RAIN</h2>,
+            <h2 className='card-description'>Descripcion del producto. Este es un texto simulado</h2>
+            <p className='card-ver mas'>VER mas</p>
+
+          </div>
+       </div>
+
+    </div>
+      <button className='card__btn'>View more</button>
+
+  </div>
+  )
+
+}    
+
+
   return (
     <div className="App">
       <Header />
       <Slider images={images}/>
+      <Card />
     </div>
   );
 }
