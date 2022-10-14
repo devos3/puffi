504665dbb7c5a89dab3756f5c3c6f9b7cac7b369
L3x <hleon0507@gmail.com>
2022-10-13 21:52:43 -0500

    SLIDER FUNCIONANDO PENDIENTE MEJORES FOTOS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { Header }  from './components/common'
+import Slider from './components/Slider'
 import './App.css';
 
 function App() {
+  const images = [
+    {
+      id:'1',
+      title:'Para tus hijos',
+      image:'https://m.media-amazon.com/images/I/61uI0MGpKvL._AC_SL1001_.jpg',
+    },
+    {
+      id:'2',
+      title:'Descanso merecido',
+      image:'https://m.media-amazon.com/images/I/61Y-HZ4dPLL._AC_SL1024_.jpg',
+    },
+    {
+      id:'3',
+      title:'Para ti',
+      image:'https://m.media-amazon.com/images/I/71tQckGUpFL._AC_SL1500_.jpg',
+    }
+
+
+  ]
   return (
     <div className="App">
       <Header />
+      <Slider images={images}/>
     </div>
   );
 }
--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { Navbar } from '../../common';
 
+import './Header.css';
+
 function Header (){
     return(
         <section className="header">
-            <section className="header-top">
-              <section className="header-top_logo">
-                <a href="/" className="header-logo">LOGO</a> 
-              </section>
-              <section className="header-top_navbar">
-                <Navbar />
-              </section>
+          <section className="header-top">
+            <section className="header-top__logo">
+              <a href="/" className="header-logo">LOGO</a> 
             </section>
-            <section className="header-bottom">
-              <section className="header-bottom_phone">
-                000000122121
+              <section className="header-top__navbar">
+                <section className="header-top__navigation">
+                  <Navbar />
+                </section>
+                <hr className="header-top__separator"/>
               </section>
-              <section className="header-bottom_email">
-                lex.info@gmail.com
-              </section>   
+          </section>
+          <section className="header-bottom">
+            <section className="header-bottom__phone">
+              000000122121
             </section>
-        </section>
+            <section className="header-bottom__email">
+              lex.info@gmail.com
+            </section>   
+          </section>
+      </section>
     )
 }
 
